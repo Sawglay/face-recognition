3706bf0230e701d96dcba484262cf7996ed61d6f
Hlaing Min Han <125644169+Sawglay@users.noreply.github.com>
2026-07-09 22:27:25 +0700

    Create FaceBiometricRegistry.java with imports

---
+++ java/FaceBiometricRegistry.java
@@ -0,0 +1,13 @@
+package java;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
